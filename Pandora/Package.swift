f7063777b38b03a9055a43dba99a46c5ea5354ef
Tiago Silva <tiagomssantossilva@gmail.com>
2025-03-08 23:56:41 +0000

    Pandora package creation

---
+++ Pandora/Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Pandora",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Pandora",
+            targets: [
+                "Pandora"
+            ]),
+    ],
+    dependencies: [
+        .package(path: "../LeboncoinUIKit")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Pandora",
+            dependencies: [
+                "LeboncoinUIKit"
+            ],
+            sources: [
+                "Public",
+                "Classes"
+            ]
+        ),
+        .testTarget(
+            name: "PandoraTests",
+            dependencies: ["Pandora"]
+        ),
+    ]
+)
--- Pandora/Sources/Public/NavigationController.swift
+++ Pandora/Sources/Public/NavigationController.swift
@@ -7,8 +7,8 @@
 
 import UIKit
 
-final class NavigationController: UINavigationController {
-    override init(rootViewController: UIViewController) {
+public final class NavigationController: UINavigationController {
+    public override init(rootViewController: UIViewController) {
         super.init(navigationBarClass: NavigationBar.self, toolbarClass: nil)
         self.setViewControllers([rootViewController], animated: false)
     }
